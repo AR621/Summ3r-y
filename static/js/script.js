3b626fb2ba03a339b85de10fb8e5c0510de2d69e
colonelWalterKurtz <253264@student.pwr.edu.pl>
2022-12-15 16:16:29 +0100

    adding some sweet animations

---
+++ static/js/script.js
@@ -0,0 +1,7 @@
+function uploadButton(){
+    document.getElementById("audio-label-text").innerHTML="keep waiting :-)"
+}
+
+function summarizeButton(){
+    document.getElementById("url-label").innerHTML="it' gonna take a while, just keep waiting :-)"
+}
