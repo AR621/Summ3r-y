6feaee5bef63b648400b9267bbc50c59b8efc850
Patryk Dolacinski <60547627+AR621@users.noreply.github.com>
2023-01-16 18:17:44 +0100

    about update

--- summarizer.py
+++ summarizer.py
@@ -3,16 +3,18 @@ import os
 
 
 def request_summary(input_text, temperature=0.1, max_tokens=768):
+    print(input_text)
     openai.api_key = os.getenv("OPENAI_API_KEY")
     summary = ""
     for text_partition in input_text:
-        response = openai.Completion.create(
-            max_tokens=max_tokens,
-            model="text-curie-001",
-            prompt=summarize_prompt(text_partition),
-            temperature=temperature,
-        )
-        summary = summary + str(response.choices[0].text)
+        if text_partition != '':
+            response = openai.Completion.create(
+                max_tokens=max_tokens,
+                model="text-curie-001",
+                prompt=summarize_prompt(text_partition),
+                temperature=temperature,
+            )
+            summary = summary + str(response.choices[0].text)
 
     return summary
 
