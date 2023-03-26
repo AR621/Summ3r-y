df96b0d06294427d1db2ab16bed932b2793c31ad
colonelWalterKurtz <mateusz.gancarek@outlook.com>
2023-03-26 17:56:56 +0200

    setup.py checks if project structure is valid for running

--- setup.py
+++ setup.py
@@ -1,2 +1,15 @@
 from summ3ry.app import app
+import os
+
+def project_directory():
+    summ3ry_structure = (os.listdir(os.path.join(os.getcwd(), 'summ3ry')))
+    # print(summ3ry_structure)
+    if 'text' not in summ3ry_structure:
+        os.makedirs(os.path.join(os.path.join(os.getcwd(), 'summ3ry' ), 'text'))
+    if 'downloads' not in summ3ry_structure:    
+        os.makedirs(os.path.join(os.path.join(os.getcwd(), 'summ3ry' ), 'downloads'))
+    if 'uploads' not in summ3ry_structure:
+        os.makedirs(os.path.join(os.path.join(os.getcwd(), 'summ3ry' ), 'uploads'))
+
+project_directory()
 app.run(debug=True)
